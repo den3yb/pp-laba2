a3d8e3da20ed3a08268d9663d4e17fc85e53184e
den3yb <den3yb@gmail.com>
2023-12-05 21:20:02 +0400

    first commit

---
+++ 1/exercise1.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+def get_rel(absolut:str,file:str) -> str:
+    path = os.path.join(os.path.commonpath([absolut,file]))
+    rel=absolut.replace(path,"")
+    return rel
+    
+
+def main(dataset: str) -> None:
+    path_f = os.path.dirname(__file__)
+    f=open("annotation.csv", "w")
+    writer=csv.writer(f,delimiter=',',lineterminator='\n')
+    for i in os.listdir(dataset):
+        fold = os.path.join(dataset, i)
+        for otz_num in os.listdir(fold):
+            absolute=os.path.join(fold,otz_num)
+            relative=get_rel(absolute, path_f)
+            writer.writerow([absolute,relative,i])
+
+        
+ 
+
+if __name__ == '__main__':
+    main("C:\Proganiy\Git PP\dataset")
