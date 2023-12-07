effca25810b24c93b0380ee0a11e9ebc297b0cde
den3yb <den3yb@gmail.com>
2023-12-07 08:38:41 +0400

    Отформатированные

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -3,30 +3,35 @@ import csv
 import shutil
 import random
 
-def get_rel(file_path:str,copy_path:str) -> str:
-    path = os.path.join(os.path.commonpath([file_path,copy_path]),"")
-    rel=copy_path.replace(path,"")
+
+def get_rel(file_path: str, copy_path: str) -> str:
+    path = os.path.join(os.path.commonpath([file_path, copy_path]), "")
+    rel = copy_path.replace(path, "")
     return rel
 
 
-def main(dataset:str,copy_path:str,name:str):
+def main(dataset: str, copy_path: str, name: str):
     file_path = os.path.dirname(__file__)
     if not os.path.exists(copy_path):
         os.makedirs(copy_path)
-    f=open(name, "w")
-    writer=csv.writer(f,delimiter=',',lineterminator='\n')
-    random_list = random.sample(list(range(1,10000)),3000)
+    f = open(name, "w")
+    writer = csv.writer(f, delimiter=",", lineterminator="\n")
+    random_list = random.sample(list(range(1, 10000)), 3000)
     count = 0
-    writer=csv.writer(f,delimiter=',',lineterminator='\n')
+    writer = csv.writer(f, delimiter=",", lineterminator="\n")
     for fold in os.listdir(dataset):
-        for file in os.listdir(os.path.join(dataset,fold)):
-            orig = os.path.join(dataset,fold,file)
-            copy = os.path.join(copy_path,str(random_list[count]))
+        for file in os.listdir(os.path.join(dataset, fold)):
+            orig = os.path.join(dataset, fold, file)
+            copy = os.path.join(copy_path, str(random_list[count]))
             count += 1
             relative = get_rel(file_path, copy)
             shutil.copyfile(orig, copy)
-            writer.writerow([orig,relative,fold])
+            writer.writerow([orig, relative, fold])
 
 
-if __name__ == '__main__':
-    main("C:\\Proganiy\\Git PP\\dataset","C:\\Proganiy\\pp-laba2\\dataset_random","annotation_for_random.csv")
+if __name__ == "__main__":
+    main(
+        "C:\\Proganiy\\Git PP\\dataset",
+        "C:\\Proganiy\\pp-laba2\\dataset_random",
+        "annotation_for_random.csv",
+    )
--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -2,30 +2,29 @@ import os
 
 from exercise4 import get
 
+
 class otzovik:
-    def __init__(self,dataset:str,rait:str):
+    def __init__(self, dataset: str, rait: str):
         self.counter = 0
         self.dataset = dataset
         self.rait = rait
         self.limit = 0
-        self.limit += len(os.listdir(os.path.join(dataset,rait)))
+        self.limit += len(os.listdir(os.path.join(dataset, rait)))
         print("LIMIT:", self.limit)
-        
-            
-            
-       
+
     def __iter__(self):
-        return self    
-        
+        return self
+
     def __next__(self):
         if self.counter < self.limit:
-            temp = get(self.dataset,self.rait, self.counter)
+            temp = get(self.dataset, self.rait, self.counter)
             self.counter += 1
             return temp
         else:
             raise StopIteration
 
-test = otzovik("C:\Proganiy\Git PP\dataset","5")
+
+test = otzovik("C:\Proganiy\Git PP\dataset", "4")
 
 for val in test:
     print(val)
